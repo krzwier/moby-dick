4c0fd2405021b7ddb384533d19e6240e74fc0f87
krzwier <krzwier@gmail.com>
2020-03-19 20:59:29 -0500

    Add short-text.txt as test file. New test case for opening and reading short-text.txt. Test passes.

--- src/main/WordCounter.java
+++ src/main/WordCounter.java
@@ -1,4 +1,28 @@
 package main;
 
+import java.io.*;
+//import java.util.HashMap;
+//import java.util.Map;
+
 public class WordCounter {
+
+    /* private Map<String, Integer> wordMap = new HashMap<>(); */
+
+
+    /**
+     * @param   filename    name of file to read
+     * @return              boolean indicating success
+     */
+    public boolean LoadWords(String filename) {
+        boolean success = false;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            br.close();
+            success = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return success;
+    }
+
 }
--- src/test/TestWordCounter.java
+++ src/test/TestWordCounter.java
@@ -1,4 +1,38 @@
 package test;
 
+import main.WordCounter;
+import org.junit.jupiter.api.Test;
+
+//import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestWordCounter {
+
+    @Test
+    void test_loadwordsforshorttextfile(){
+        WordCounter wordCounter = new WordCounter();
+        assertTrue(wordCounter.LoadWords("short-text.txt"));
+    }
+
+    /*
+    @Test
+    void test_wordlistforshorttextfile(){
+        //WordCounter wordCounter = new WordCounter();
+        HashMap<String,Integer> wordMap = new HashMap<>();
+        wordMap.put("this", 1);
+        wordMap.put("is", 1);
+        wordMap.put("a", 1);
+        wordMap.put("dummy", 1);
+        wordMap.put("file", 1);
+        wordMap.put("for", 1);
+        wordMap.put("test", 1);
+        wordMap.put("purposes", 1);
+
+        //assertTrue(wordMap.equals(wordCounter.getWordMap()));
+
+
+    }
+    */
+
 }
