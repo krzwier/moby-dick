a21ac25a528552fd11fb41db13615d5ee95dded5
krzwier <krzwier@gmail.com>
2020-03-19 22:57:30 -0500

    Add method to load stop words.  Change LoadWords method to check against stop words.  Adjust test cases so that they don't include stop words.  All test cases pass.

--- src/main/WordCounter.java
+++ src/main/WordCounter.java
@@ -1,19 +1,26 @@
 package main;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class WordCounter {
 
     private boolean wordsLoaded;
+    private boolean stopWordsLoaded;
+    private HashSet<String> stopWords = new HashSet<>();
     private HashMap<String, Integer> wordMap = new HashMap<>();
 
     /**
-     * Creates a new WordCounter object by loading words from given file.
+     * Creates a new WordCounter object by loading stop words and
+     * words from given file. Sets boolean fields to indicate success
+     * of load methods.
      *
      * @param   filename    name of file to load
      */
     public WordCounter(String filename) {
+        this.stopWordsLoaded = LoadStopWords("stop-words.txt");
         this.wordsLoaded = LoadWords(filename);
     }
 
@@ -25,12 +32,44 @@ public class WordCounter {
     }
 
     /**
+     * @return boolean variable indicating if stop words have been loaded
+     */
+    public boolean getStopWordsLoaded(){
+        return this.stopWordsLoaded;
+    }
+
+    /**
      * @return WordMap with all words and counts
      */
     public HashMap<String, Integer> getWordMap() {
         return this.wordMap;
     }
 
+    /**
+     * Opens stop words file and reads each word, line by line.
+     * Stores stop words in stopWords hashset
+     *
+     * @param   filename    name of file to read
+     * @return              boolean indicating success
+     */
+    private boolean LoadStopWords(String filename) {
+        boolean success = false;
+        String word;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            while ((word = br.readLine()) != null) {
+                String lowercaseWord = word.toLowerCase();
+                if (!lowercaseWord.isEmpty() && (lowercaseWord.charAt(0) != '#')){
+                    stopWords.add(lowercaseWord);
+                }
+            }
+            br.close();
+            success = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return success;
+    }
 
     /**
      * Opens file and reads line by line, separating each line in to words.
@@ -48,7 +87,8 @@ public class WordCounter {
                 String[] words = line.split("\\W+");
                 for (String s : words) {
                     String lowercaseWord = s.toLowerCase();
-                    if (wordMap.putIfAbsent(lowercaseWord,1) != null){
+                    if (!stopWords.contains(lowercaseWord) &&
+                            wordMap.putIfAbsent(lowercaseWord,1) != null){
                         wordMap.replace(lowercaseWord,wordMap.get(lowercaseWord) + 1);
                     }
                 }
--- src/test/TestWordCounter.java
+++ src/test/TestWordCounter.java
@@ -13,18 +13,19 @@ public class TestWordCounter {
     void test_loadwordsforonelinetextfile(){
         WordCounter wordCounter = new WordCounter("short-text.txt");
         assertTrue(wordCounter.getWordsLoaded());
+        assertTrue(wordCounter.getStopWordsLoaded());
     }
 
     @Test
     void test_wordlistforonelinetextfile(){
         WordCounter wordCounter = new WordCounter("short-text.txt");
         HashMap<String,Integer> wordMap = new HashMap<>();
-        wordMap.put("this", 1);
-        wordMap.put("is", 1);
-        wordMap.put("a", 1);
+        //wordMap.put("this", 1);
+        //wordMap.put("is", 1);
+        //wordMap.put("a", 1);
         wordMap.put("dummy", 1);
         wordMap.put("file", 1);
-        wordMap.put("for", 1);
+        //wordMap.put("for", 1);
         wordMap.put("test", 1);
         wordMap.put("purposes", 1);
 
@@ -35,18 +36,18 @@ public class TestWordCounter {
     void test_wordlistfortwolinetextfile(){
         WordCounter wordCounter = new WordCounter("two-line-text.txt");
         HashMap<String,Integer> wordMap = new HashMap<>();
-        wordMap.put("this", 2);
-        wordMap.put("is", 2);
-        wordMap.put("a", 2);
+        //wordMap.put("this", 2);
+        //wordMap.put("is", 2);
+        //wordMap.put("a", 2);
         wordMap.put("dummy", 1);
         wordMap.put("file", 1);
-        wordMap.put("for", 1);
+        //wordMap.put("for", 1);
         wordMap.put("test", 1);
         wordMap.put("purposes", 1);
-        wordMap.put("but", 1);
+        //wordMap.put("but", 1);
         wordMap.put("surprise", 1);
-        wordMap.put("there", 1);
-        wordMap.put("second", 1);
+        //wordMap.put("there", 1);
+        //wordMap.put("second", 1);
         wordMap.put("line", 1);
         wordMap.put("time", 1);
 
