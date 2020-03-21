336d7daea55b647ef82777cf9ccbeb91f1d41961
krzwier <krzwier@gmail.com>
2020-03-20 19:21:43 -0500

    Separate Sorter class into its own file and add TableSaw dependency for creating plots.

---
+++ src/main/java/Sorter.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+import java.util.Map;
+
+public class Sorter implements Comparator<Map.Entry<String,Integer>> {
+    // complete this method
+    public int compare(Map.Entry<String, Integer> word1, Map.Entry<String,Integer> word2) {
+        if (!word1.getValue().equals(word2.getValue())) {
+            return word2.getValue() - word1.getValue();
+        } else {
+            return (word1.getKey()).compareTo(word2.getKey());
+        }
+    }
+}
